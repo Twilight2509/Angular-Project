9b2ae8930b97cdf562f3515b00d3e9f084670186
Twilight2509 <onlynanh@gmail.com>
2025-09-26 11:51:03 +0700

    dashboard setup

--- todo-myself/src/app/login/login.component.ts
+++ todo-myself/src/app/login/login.component.ts
@@ -1,37 +1,34 @@
-import { Component } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, NzButtonModule, NzCheckboxModule, NzFormModule, NzInputModule],
+  imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule], // Add ReactiveFormsModule
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  validateForm = this.fb.group({
-    username: this.fb.control('', [Validators.required]),
-    password: this.fb.control('', [Validators.required]),
-    remember: this.fb.control(true)
-  });
+export class LoginComponent implements OnInit {
+  validateForm: any;
+
+  constructor(private fb: NonNullableFormBuilder) {
+    this.validateForm = this.fb.group({
+      username: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required]),
+      remember: this.fb.control(true)
+    });
+  }
+
+  ngOnInit(): void {}
 
   submitForm(): void {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
     } else {
-      Object.values(this.validateForm.controls).forEach(control => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
-        }
-      });
+      console.log('form invalid');
     }
   }
-
-  constructor(private fb: NonNullableFormBuilder) {}
 }
